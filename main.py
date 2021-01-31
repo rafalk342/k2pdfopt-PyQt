bbba72cab1cbabace73f2d59d9d79a533c15078b
rafalk342 <rafalk342@gmail.com>
2021-01-31 17:34:22 +0100

    Refactor names.

--- main.py
+++ main.py
@@ -19,32 +19,32 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        self.options = Options(self.deviceComboBox)
-        self.converter = Converter(event_loop, self.logText, self.options, self.tabWidget, self.inputFilePath)
-        self.preview = Preview(self.converter, self.imagePreview, self.previewLineEdit)
+        self.options = Options(self.device_combo_box)
+        self.converter = Converter(event_loop, self.logText, self.options, self.tab_widget, self.inputFilePath)
+        self.preview = Preview(self.converter, self.image_preview, self.preview_line_edit)
         self.set_up_toggles()
         self.set_up_options()
         self.set_up_margin_option()
-        self.chooseFileButton.pressed.connect(self.choose_file)
+        self.choose_file_button.pressed.connect(self.choose_file)
         self.convertFileButton.pressed.connect(self.convert)
-        self.deviceComboBox.setModel(DeviceComboBoxModel())
-        self.deviceComboBox.setCurrentIndex(self.options.get_device_index())
-        self.deviceComboBox.activated.connect(self.device_changed)
-        self.tabWidget.setCurrentIndex(0)
-        self.previewButton.clicked.connect(self.preview.handle_preview_button_clicked)
+        self.device_combo_box.setModel(DeviceComboBoxModel())
+        self.device_combo_box.setCurrentIndex(self.options.get_device_index())
+        self.device_combo_box.activated.connect(self.device_changed)
+        self.tab_widget.setCurrentIndex(0)
+        self.preview_button.clicked.connect(self.preview.handle_preview_button_clicked)
         self.set_up_validators()
 
     def set_up_toggles(self):
         toggles_map = {
-            '-as': self.autostraightenCheckBox,
-            '-bp': self.breakAfterEachCheckBox,
-            '-c': self.colorOutputCheckBox,
-            '-ls': self.landscapeCheckBox,
-            '-n': self.nativePDFOutputCheckBox,
-            '-r': self.rightToLeftCheckBox,
-            '-sm': self.generateMarkedUpSourceCheckBox,
-            '-wrap': self.reFlowTextCheckBox,
-            '-evl 1': self.eraseVerticalLinesCheckBox
+            '-as': self.autostraighten_check_box,
+            '-bp': self.break_after_each_check_box,
+            '-c': self.color_output_check_box,
+            '-ls': self.landscape_check_box,
+            '-n': self.native_PDF_output_check_box,
+            '-r': self.right_to_left_check_box,
+            '-sm': self.generate_marked_up_source_check_box,
+            '-wrap': self.re_flow_text_check_box,
+            '-evl 1': self.erase_vertical_lines_check_box
         }
         for name, checkbox in toggles_map.items():
             checkbox.setChecked(self.options.is_option_active(name))
@@ -53,9 +53,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def set_up_options(self):
         options_map = {
-            '-col': {'checkbox': self.maxColumnsCheckBox, 'line_edit': self.maxColumnsLineEdit},
-            '-dr': {'checkbox': self.resolutionFactorCheckBox, 'line_edit': self.resolutionFactorLineEdit},
-            '-p': {'checkbox': None, 'line_edit': self.pageRangeLineEdit}
+            '-col': {'checkbox': self.max_columns_check_box, 'line_edit': self.max_columns_line_edit},
+            '-dr': {'checkbox': self.resolution_factor_check_box, 'line_edit': self.resolution_factor_line_edit},
+            '-p': {'checkbox': None, 'line_edit': self.page_range_line_edit}
         }
         for option_name, option_dict in options_map.items():
             checkbox = option_dict['checkbox']
@@ -70,45 +70,45 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 lambda text, option_name=option_name: self.options.change_option_value(option_name, text))
 
     def set_up_margin_option(self):
-        self.cropMarginCheckBox.setChecked(self.options.is_option_active('-m'))
-        self.leftMarginLineEdit.setText(self.options.get_margin_value('left'))
-        self.topMarginLineEdit.setText(self.options.get_margin_value('top'))
-        self.rightMarginLineEdit.setText(self.options.get_margin_value('right'))
-        self.bottomMarginLineEdit.setText(self.options.get_margin_value('bottom'))
-        self.cropMarginCheckBox.stateChanged.connect(
-            lambda: self.options.toggle_changed(self.cropMarginCheckBox, '-m'))
-        self.leftMarginLineEdit.textChanged.connect(
+        self.crop_margin_check_box.setChecked(self.options.is_option_active('-m'))
+        self.left_margin_line_edit.setText(self.options.get_margin_value('left'))
+        self.top_margin_line_edit.setText(self.options.get_margin_value('top'))
+        self.right_margin_line_edit.setText(self.options.get_margin_value('right'))
+        self.bottom_margin_line_edit.setText(self.options.get_margin_value('bottom'))
+        self.crop_margin_check_box.stateChanged.connect(
+            lambda: self.options.toggle_changed(self.crop_margin_check_box, '-m'))
+        self.left_margin_line_edit.textChanged.connect(
             lambda text: self.options.change_margin_option('left', text))
-        self.topMarginLineEdit.textChanged.connect(
+        self.top_margin_line_edit.textChanged.connect(
             lambda text: self.options.change_margin_option('top', text))
-        self.rightMarginLineEdit.textChanged.connect(
+        self.right_margin_line_edit.textChanged.connect(
             lambda text: self.options.change_margin_option('right', text))
-        self.bottomMarginLineEdit.textChanged.connect(
+        self.bottom_margin_line_edit.textChanged.connect(
             lambda text: self.options.change_margin_option('bottom', text))
 
     def set_up_validators(self):
         reg_exp_whole_number = QRegExp(r'\d*')
-        whole_number_inputs = [self.maxColumnsLineEdit, self.leftMarginLineEdit, self.topMarginLineEdit,
-                               self.rightMarginLineEdit, self.bottomMarginLineEdit, self.previewLineEdit]
+        whole_number_inputs = [self.max_columns_line_edit, self.left_margin_line_edit, self.top_margin_line_edit,
+                               self.right_margin_line_edit, self.bottom_margin_line_edit, self.preview_line_edit]
         for line_edit in whole_number_inputs:
             line_edit_validator = QRegExpValidator(reg_exp_whole_number, line_edit)
             line_edit.setValidator(line_edit_validator)
 
         reg_exp_float = QRegExp(r'[0-9]+.?[0-9]+')
-        resolution_factor_validator = QRegExpValidator(reg_exp_float, self.resolutionFactorLineEdit)
-        self.resolutionFactorLineEdit.setValidator(resolution_factor_validator)
+        resolution_factor_validator = QRegExpValidator(reg_exp_float, self.resolution_factor_line_edit)
+        self.resolution_factor_line_edit.setValidator(resolution_factor_validator)
 
         reg_exp_page_range = QRegExp(r'\d+-?\d*')
-        page_range_validator = QRegExpValidator(reg_exp_page_range, self.pageRangeLineEdit)
-        self.pageRangeLineEdit.setValidator(page_range_validator)
+        page_range_validator = QRegExpValidator(reg_exp_page_range, self.page_range_line_edit)
+        self.page_range_line_edit.setValidator(page_range_validator)
 
     def choose_file(self):
         file_path, _ = QFileDialog.getOpenFileName(self, 'Open file', './', "PDF/DjVu files (*.pdf *.djvu)")
         self.inputFilePath.setText(file_path)
 
     def device_changed(self, index):
-        text = self.deviceComboBox.itemText(index)
-        data = self.deviceComboBox.itemData(index, QtCore.Qt.UserRole)
+        text = self.device_combo_box.itemText(index)
+        data = self.device_combo_box.itemData(index, QtCore.Qt.UserRole)
         print('User chose {} which corresponds to {}'.format(text, data))
         self.options.set_device_index(index)
 
@@ -119,8 +119,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    event_loop = QEventLoop(app)
-    asyncio.set_event_loop(event_loop)
-    window = MainWindow(event_loop)
+    app_event_loop = QEventLoop(app)
+    asyncio.set_event_loop(app_event_loop)
+    window = MainWindow(app_event_loop)
     window.show()
     app.exec_()
